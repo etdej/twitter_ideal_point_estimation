456347b76ee42b9f831bd300069a05bd902044f1
Etienne Dejoie <etiennedejoie@gmail.com>
2017-10-10 11:14:23 -0400

    continue playing around

--- get_data/get_data.py
+++ get_data/get_data.py
@@ -3,19 +3,33 @@ import config
 
 class twitter():
     def __init__(self):
-        print(config.consumer_key)
         auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
         auth.set_access_token(config.access_token, config.access_token_secret)
         self.api = tweepy.API(auth)
 
-    def get_followers(self):
-        return
+    def get_user_id(self, user_name):
+        user = self.api.get_user(user_name)
+        return user.id
+
+    def get_followers(self, id):
+        followers = tweet.api.followers_ids(id)
+        for follower in followers:
+            print(follower)
+
 
     def get_tweets(self):
         public_tweets = self.api.home_timeline()
         for tweet in public_tweets:
           print(tweet.text)
 
+    def save_edge(self, id, file):
+        followers = tweet.api.followers_ids(id)
+        for follower in followers:
+            print(follower)
+        return followers
+            
 if __name__ == "__main__":
     tweet = twitter()
-    tweet.get_tweets()
+    id = tweet.get_user_id("alannunnelee")
+    print("id = "+str(id))
+    print(len(tweet.api.followers_ids(id)))
